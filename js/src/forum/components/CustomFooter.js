7617e6d17fb9227d076cbbee719697a0ed15ed12
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2024-12-06 13:45:50 +0300

    Update CustomFooter.js

--- js/src/forum/components/CustomFooter.js
+++ js/src/forum/components/CustomFooter.js
@@ -1,12 +1,12 @@
 import app from 'flarum/forum/app';
 import Component from 'flarum/common/Component';
 
-export default class CustomFooter extends Component {
+export default class ModernFooter extends Component {
   oninit(vnode) {
     super.oninit(vnode);
     this.open = false;
 
-    const js = app.forum.attribute('fof-custom-footer.js');
+    const js = app.forum.attribute('huseyinfiliz-modern-footer.js');
     if (js) {
       $('body').append(`<script>${js.trim()}</script>`);
     }
@@ -14,10 +14,10 @@ export default class CustomFooter extends Component {
 
   view() {
     return (
-      <div id="CustomFooter" className={this.open && 'showing'}>
+      <div id="ModernFooter" className={this.open && 'showing'}>
         <div className="container">
-          <div className="Footer--Content" style={`height: ${this.open ? app.forum.attribute('fof-custom-footer.height') || 50 : 0}px;`}>
-            {m.trust(app.forum.attribute('fof-custom-footer.text') || '')}
+          <div className="Footer--Content" style={`height: ${this.open ? app.forum.attribute('huseyinfiliz-modern-footer.height') || 50 : 0}px;`}>
+            {m.trust(app.forum.attribute('huseyinfiliz-modern-footer.text') || '')}
           </div>
           <div className="Footer--Icons">
             <i className={`Footer--Show fas fa-info-circle ${this.open && 'hidden'}`} onclick={() => (this.open = true)} />
