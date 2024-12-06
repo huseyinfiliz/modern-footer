a0cfe969154f05973f6d563d00398c2fdff5d8af
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2024-12-06 16:10:59 +0300

    Update index.ts

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -5,19 +5,19 @@ app.initializers.add('huseyinfiliz-modern-footer', () => {
     .for('huseyinfiliz-modern-footer')
     .registerSetting({
       label: app.translator.trans('huseyinfiliz-modern-footer.admin.settings.text'),
-      setting: 'huseyinfiliz-modern-footer.text',
+      setting: 'modern-footer.text',
       type: 'text',
     })
     .registerSetting({
       label: app.translator.trans('huseyinfiliz-modern-footer.admin.settings.height'),
-      setting: 'huseyinfiliz-modern-footer.height',
+      setting: 'modern-footer.height',
       placeholder: '50',
       type: 'number',
       simple: 'simple',
     })
     .registerSetting({
       label: app.translator.trans('huseyinfiliz-modern-footer.admin.settings.js'),
-      setting: 'huseyinfiliz-modern-footer.js',
+      setting: 'modern-footer.js',
       type: 'text',
     });
 });
