9ea07246d6300ec00c52fc1add36b2d10a81bf8f
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2024-12-06 14:47:32 +0300

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,12 +2,12 @@ import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import ForumApplication from 'flarum/forum/ForumApplication';
 
-import CustomFooter from './components/ModernFooter';
+import CustomFooter from './components/CustomFooter';
 
 app.initializers.add('huseyinfiliz-modern-footer', () => {
   extend(ForumApplication.prototype, 'mount', () => {
     const footer = document.createElement('div');
 
-    m.mount(document.body.appendChild(footer), ModernFooter);
+    m.mount(document.body.appendChild(footer), CustomFooter);
   });
 });
