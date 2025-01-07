821c907ea358ab7ffe5c35324b50bebe93126765
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2025-01-07 16:15:34 +0300

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -7,19 +7,7 @@ import CustomFooter from './components/CustomFooter';
 app.initializers.add('huseyinfiliz-modern-footer', () => {
   extend(ForumApplication.prototype, 'mount', () => {
     const footer = document.createElement('footer');
-
-    const mobileTabHeight = app.forum.attribute('modern-footer.mobile-tab') || '0px';
-
-    const style = document.createElement('style');
-    style.innerHTML = `
-      @media (max-width: 768px) {
-        footer {
-          padding-bottom: ${mobileTabHeight};
-        }
-      }
-    `;
-    document.head.appendChild(style);
-
+    footer.id = 'modern-footer';
     m.mount(document.body.appendChild(footer), CustomFooter);
   });
 });
