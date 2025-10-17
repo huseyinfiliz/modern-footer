053079ed29d3ce9d3b20ebddc00ba52c031ef428
Hüseyin Filiz <mysuperuser01@gmail.com>
2025-10-17 14:10:20 +0300

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,5 +2,5 @@ import app from 'flarum/admin/app';
 import SettingsPage from './Pages/SettingsPage';
 
 app.initializers.add('huseyinfiliz-modern-footer', () => {
-  app.extensionData.for('huseyinfiliz-modern-footer').registerPage(SettingsPage);
+  app.registry.for('huseyinfiliz-modern-footer').registerPage(SettingsPage);
 });
